7249917a7ae27f63b045972a310ccca64b8090c8
jvugar <jvugar@bu.edu>
2019-06-09 07:31:09 -0400

    Add files via upload

--- get_intersections.py
+++ get_intersections.py
@@ -4,22 +4,37 @@ import requests, json
 import pandas as pd
 from pandas import ExcelWriter
 import time
+import argparse
 
 
 #input example:
 #python <name_of_the_script> <file_name_to_read_from>
-#python get_intersections.py Allston-Village-Business-Inventory.csv 
+#python get_intersections.py --spreadsheet Allston-Village-Business-Inventory.csv --city Boston --state MA 
+#city and state are optional parameters
 
 #output:
-#an excel file with intersections with longitude and latitude
-#spark_output_intersections.csv
+#a csv file with intersections with longitude and latitude
+#intersections_spark_output.csv
 
 
-
-name_of_script = sys.argv[0]
-filename = sys.argv[1]
+input = argparse.ArgumentParser()
+input.add_argument("-s", "--spreadsheet", required=True, help="Spreadsheet consisting of addresses")
+input.add_argument("-c", "--city", type =str, default="Boston", help="city in which we are interested")
+input.add_argument("-st", "--state", type =str, default="MA", help="state where the city of interest is located")
+arguments = vars(input.parse_args())
 #reading csv file
-df = pd.read_csv(filename)
+df = pd.read_csv(arguments['spreadsheet'])
+spreadsheet_name = str(arguments['spreadsheet'])
+spreadsheet_name = "intersections_" + spreadsheet_name
+
+
+longitude = []         #store longitude of the addresses
+latitude = []          #store latitude of the addresses
+intersection_name = []
+intersection_long = []
+intersection_lat = []
+city = arguments["city"]
+state = arguments["state"]
 
 #reading csv for Chinatown; this problem is caused by excel files not following same format for all mainstreets
 #do it in the case if the columns aren't already named in the csv file or you want to change the names
@@ -40,8 +55,9 @@ df = pd.read_csv(filename)
 #for Bowdoin, Mission Hill
 # list_columns = list(df.columns)  #for debugging purposes in case if there is a problem with reading a column
 # print(list_columns)
-df['true_address'] = df['Address']
-df['true_address'].replace(" ","+",regex=True)   #right format for the API request
+def formatted_address():
+    df['true_address'] = df['Billing Street']
+    df['true_address'].replace(" ","+",regex=True)   #right format for the API request
 
 #for Brighton, Chinatown, Fields Corner, Four Corners
 # df['true_address'] = df['Unnamed: 5']
@@ -49,62 +65,66 @@ df['true_address'].replace(" ","+",regex=True)   #right format for the API reque
 # df['true_address'].replace(",","+",regex=True)
 
 
+#use this to get the longitude and latitude of the addresses provided in the spreadsheet
+def address_longitude():
+    my_api_key = ''     				#your Google API key for geocode here  
+    									#Google API key for getting longitude and latitude for an address
+                                                                 #5$ per 1,000 request
 
-
-my_api_key = ''     				#your Google API key for geocode here  
-									#Google API key for getting longitude and latitude for an address
-                                                             #5$ per 1,000 request
-
-url_main = "https://maps.googleapis.com/maps/api/geocode/json?address="
-url_last = ",+Boston,+MA&key=" + my_api_key
-longitude = []         #store longitude of the addresses
-latitude = []          #store latitude of the addresses
-for i in range(len(df)):
-    url_request = url_main + str(df['true_address'][i]) + url_last
-    r_test = requests.get(url_request)
-    x = r_test.json()
-    if x['status'] == 'OK':
-        longitude.append(x['results'][0]['geometry']['location']['lng'])
-        latitude.append(x['results'][0]['geometry']['location']['lat']) 
-
-
-result_first = 'http://api.geonames.org/findNearestIntersectionJSON?lat=' #find the nearest intersection for a given longitude and latitude pair
-result_last = '&username='                                          #your geoname username here
-intersection_name = []
-intersection_long = []
-intersection_lat = []
-for i in range(len(longitude)):
-    url_final = result_first + str(latitude[i]) + '&lng=' + str(longitude[i]) + result_last
-    r_test = requests.get(url_final).json()
-    if "intersection" in r_test.keys():     #making sure that there is no error in the output
-        string_add = r_test['intersection']['street1'] + " and " + r_test['intersection']['street2'] #adding the intersection name
-        intersection_long.append(r_test['intersection']['lng'])
-        intersection_lat.append(r_test['intersection']['lat'])
-        intersection_name.append(string_add)
+    url_main = "https://maps.googleapis.com/maps/api/geocode/json?address="
+    url_last = ",+" + city + "," + "+" + state + "&key=" + my_api_key
+    for i in range(len(df)):
+        url_request = url_main + str(df['true_address'][i]) + url_last
+        r_test = requests.get(url_request)
+        x = r_test.json()
+        if x['status'] == 'OK':
+            longitude.append(x['results'][0]['geometry']['location']['lng'])
+            latitude.append(x['results'][0]['geometry']['location']['lat']) 
 
 
 
+#find the nearest intersection for a given longitude and latitude pair
+def find_intersection():
+    result_first = 'http://api.geonames.org/findNearestIntersectionJSON?lat='
+    result_last = '&username='                                          #your geoname username here
+    for i in range(len(longitude)):
+        url_final = result_first + str(latitude[i]) + '&lng=' + str(longitude[i]) + result_last
+        r_test = requests.get(url_final).json()
+        if "intersection" in r_test.keys():     #making sure that there is no error in the output
+            string_add = r_test['intersection']['street1'] + " and " + r_test['intersection']['street2'] #adding the intersection name
+            intersection_long.append(r_test['intersection']['lng'])
+            intersection_lat.append(r_test['intersection']['lat'])
+            intersection_name.append(string_add)
 
-df['longitude'] = pd.Series(longitude)    #longitude for the addresses given in the excel file
-df['latitude'] = pd.Series(latitude)     #latitude for the addresses given in the excel file
-df['intersection_long'] = pd.Series(intersection_long) #longitude for the nearest intersection for a given address
-df['intersection_lat'] = pd.Series(intersection_lat)   #latitude for the nearest intersection for a given address
-df['intersection_name'] = pd.Series(intersection_name)
 
 
+def main_function():
+    formatted_address()
+    address_longitude()
+    find_intersection()
+    df['longitude'] = pd.Series(longitude)    #longitude for the addresses given in the excel file
+    df['latitude'] = pd.Series(latitude)     #latitude for the addresses given in the excel file
+    df['intersection_long'] = pd.Series(intersection_long) #longitude for the nearest intersection for a given address
+    df['intersection_lat'] = pd.Series(intersection_lat)   #latitude for the nearest intersection for a given address
+    df['intersection_name'] = pd.Series(intersection_name)
+    df.drop_duplicates(subset='intersection_name', inplace=True)
+    df.to_csv(spreadsheet_name,index=False)
+
+
+main_function()
 #didn't find an efficient way to deal with the duplicates; if you know of a function to do all the work, please just use it
-dropped_column_list = []
-for i in range(len(df['intersection_long'])):
-    for j in range(i+1,len(df)):
-        if i not in dropped_column_list and j not in dropped_column_list:
-            if df['intersection_long'].loc[i] == df['intersection_long'].loc[j] and df['intersection_lat'].loc[i] == df['intersection_lat'].loc[j]:
-                #print("entered this condition")
-                df.drop(j,inplace=True)
-                dropped_column_list.append(j)
-df.index = range(len(df))
+# dropped_column_list = []
+# for i in range(len(df['intersection_long'])):
+#     for j in range(i+1,len(df)):
+#         if i not in dropped_column_list and j not in dropped_column_list:
+#             if df['intersection_long'].loc[i] == df['intersection_long'].loc[j] and df['intersection_lat'].loc[i] == df['intersection_lat'].loc[j]:
+#                 #print("entered this condition")
+#                 df.drop(j,inplace=True)
+#                 dropped_column_list.append(j)
+# df.index = range(len(df))
 
+#found a more efficient way to deal with duplicates
 
-df.to_csv('spark_output_intersections_Mission_Hill_final.csv',index=False)
 #if you want an Excel format comment the above line and remove comments from lines below
 # writer = ExcelWriter('spark_output_intersections.xlsx')   #storing the output here
 # df.to_excel(writer)
--- yelp_script.py
+++ yelp_script.py
@@ -4,65 +4,89 @@ import requests, json
 import pandas as pd
 from pandas import ExcelWriter
 import time
+import argparse
 
 
 #input example:
 #python <name_of_the_script> <file_name_to_read_from>
-#python final_yelp.py spark_output_intersections.csv
+#python final_yelp.py -- spreadsheet spark_output_intersections.csv --radius 1000 --results 20
+#radius and results are optional parameters 
 
 #output:
 #an excel file with intersections with longitude and latitude
 #spark_output_yelp.xlsx
 
 
+input = argparse.ArgumentParser()
+input.add_argument("-s", "--spreadsheet", required=True, help="Spreadsheet consisting of addresses")
+input.add_argument("-r", "--radius", type =int, default=1000, help="radius to search within")
+input.add_argument("-n", "--results", type =int, default=20, help="number of results to return per intersection, i.e. results*50")
+arguments = vars(input.parse_args())
+#reading csv file
+df = pd.read_csv(arguments['spreadsheet'])
 
-name_of_script = sys.argv[0]
-filename = sys.argv[1]
-df = pd.read_csv(filename)
+number_of_results = arguments["results"] #total number of businesses to be returned per intersection = number_of_results*50
+radius = arguments["radius"] #radius around the intersection that we want to get covered
+
 df = df.dropna(subset = ['intersection_name'])   #if there is an Nan value in a column of intersection_name, then drop the corresponding row
+
+
+spreadsheet_name = str(arguments['spreadsheet'])[13:-3]
+spreadsheet_name = "yelp_" + spreadsheet_name + "xlsx"
+corner_dict = {} #to store the data frames
 corners = df['intersection_name']
-corner_dict = {}    #we will store all of the dataframes in this dictionary
-for name in corners:
-    corner_dict[name] = pd.DataFrame()
+#we will store all of the dataframes in this dictionary
+def all_dictionary():
+  
+    for name in corners:
+        corner_dict[name] = pd.DataFrame()
    
     
-    
-url = 'https://api.yelp.com/v3/businesses/search'
-client_id = ''  #your client ID for yelp here
-api_key_yelp = ''  #your yelp API key here
-headers = {
-        'Authorization': 'Bearer {}'.format(api_key_yelp),
-    }
-limit = 50
-offset = 0
-number_of_results = 4 #total number of businesses to be returned per intersection = number_of_results*50
-radius = 1000 #radius around the intersection that we want to get covered
-for i in range(len(corner_dict)):
-    longitude = df['intersection_long'].loc[i]
-    latitude = df['intersection_lat'].loc[i]
-    offset = 0
-    df_added = pd.DataFrame()
-    for j in range(number_of_results):
-        
-        url_params = {'longitude':float(longitude),
-             'latitude':float(latitude),
-             'limit': limit,
-             'offset':offset,
-             'radius':radius
-                     }
-        response = requests.get(url, headers=headers, params=url_params).json()
-        if 'businesses' in response.keys():
-            new_df = pd.DataFrame.from_dict(response['businesses'])
-            df_added = df_added.append(new_df, ignore_index='True')
-            offset += 50
-        else:
-            break
-    corner_dict[corners[i]] = df_added
 
-df = pd.concat([df.assign(idx=key) for key, df in corner_dict.items()], ignore_index=True, sort=False)
-writer = ExcelWriter('spark_output_yelp_Mission_Hill_final.xlsx')
-df.to_excel(writer)
-writer.save()
+def get_businesses():   
+    url = 'https://api.yelp.com/v3/businesses/search'
+    client_id = ''  #your client ID for yelp here
+    api_key_yelp = ''  #your yelp API key here
+    headers = {
+            'Authorization': 'Bearer {}'.format(api_key_yelp),
+        }
+    limit = 50
+    offset = 0
+    for i in range(len(corner_dict)):
+        longitude = df['intersection_long'].loc[i]
+        latitude = df['intersection_lat'].loc[i]
+        offset = 0
+        df_added = pd.DataFrame()
+        for j in range(number_of_results):
+            
+            url_params = {'longitude':float(longitude),
+                 'latitude':float(latitude),
+                 'limit': limit,
+                 'offset':offset,
+                 'radius':radius
+                         }
+            response = requests.get(url, headers=headers, params=url_params).json()
+            if 'businesses' in response.keys():
+                new_df = pd.DataFrame.from_dict(response['businesses'])
+                df_added = df_added.append(new_df, ignore_index='True')
+                offset += 50
+            else:
+                break
+        corner_dict[corners[i]] = df_added
+
+
+def main_function():
+    all_dictionary()
+    get_businesses()
+    df = pd.concat([df.assign(idx=key) for key, df in corner_dict.items()], ignore_index=True, sort=False)
+    df.drop_duplicates(subset='name',inplace=True)
+    df = df[df.distance <= 1000]
+    writer = ExcelWriter(spreadsheet_name)
+    df.to_excel(writer)
+    writer.save()
+
+main_function()
+
 
 
 
